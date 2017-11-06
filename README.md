3c53451ee5e188635c781a69a4f67b3087a4fdb4
谢洋 <1192171830@qq.com>
2017-11-06 17:07:42 +0800

    1、添加测试读取配置信息实例；2、完善README

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # 工具类
 * 为自己服务
 * 便于记忆使用
+
+# How to use
+最好用最新版本：到tag那去查询
+
+```
+
+compile 'com.github.xieyang94:xyutils:0.0.1'
+
+```
+
+
+# Version
+[VERSION.md](https://github.com/xieyang94/xyutils/blob/master/VERSION.md)
+
+
+# Thank
+* Every
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- simple/src/main/java/com/xiey94/simple/MainActivity.java
+++ simple/src/main/java/com/xiey94/simple/MainActivity.java
@@ -2,8 +2,9 @@ package com.xiey94.simple;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-import com.xiey94.xyutils.R;
+import com.xiey94.xyutils.ProperUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //读取配置文件信息
+        String pwd = ProperUtil.getProperty(ProperUtil.loadProperties(this, "password.properties"), "pwd");
+        Log.d("TAG", "pwd=" + pwd);
     }
 }
